7d0da40a63fe036fc67a7b0a325a6dc698bfdef4
tomek7667 <git@cyber-man.pl>
2025-11-27 00:41:27 +0100

    make the loki logs run as goroutines

--- logger/loki.go
+++ logger/loki.go
@@ -57,48 +57,55 @@ func getRecordMessage(record slog.Record) string {
 }
 
 func (h *LokiHandler) Handle(ctx context.Context, record slog.Record) error {
-	labels := map[string]string{
-		"job":   h.appName,
-		"level": record.Level.String(),
-		"env":   utils.Getenv("ENVIRONMENT", "dev"),
-	}
+	go func() {
+		labels := map[string]string{
+			"job":   h.appName,
+			"level": record.Level.String(),
+			"env":   utils.Getenv("ENVIRONMENT", "dev"),
+		}
 
-	entry := [][]string{
-		{
-			fmt.Sprintf("%d", time.Now().UnixNano()),
-			getRecordMessage(record),
-		},
-	}
-	payload := map[string]interface{}{
-		"streams": []map[string]interface{}{
+		entry := [][]string{
 			{
-				"stream": labels,
-				"values": entry,
+				fmt.Sprintf("%d", time.Now().UnixNano()),
+				getRecordMessage(record),
 			},
-		},
-	}
-	jsonPayload, err := json.Marshal(payload)
-	if err != nil {
-		return fmt.Errorf("failed to marshal log payload: %w", err)
-	}
+		}
+		payload := map[string]interface{}{
+			"streams": []map[string]interface{}{
+				{
+					"stream": labels,
+					"values": entry,
+				},
+			},
+		}
+		jsonPayload, err := json.Marshal(payload)
+		if err != nil {
+			fmt.Printf("failed to marshal log payload: %s", err.Error())
+			return
+		}
 
-	req, err := http.NewRequest("POST", h.lokiURL, bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		return fmt.Errorf("failed to create HTTP request: %w", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	if h.username != "" && h.password != "" {
-		req.SetBasicAuth(h.username, h.password)
-	}
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to send log to Loki: %w", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusNoContent {
-		return fmt.Errorf("loki responded with status: %s", resp.Status)
-	}
+		req, err := http.NewRequest("POST", h.lokiURL, bytes.NewBuffer(jsonPayload))
+		if err != nil {
+			fmt.Printf("failed to create HTTP request: %s", err.Error())
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+		if h.username != "" && h.password != "" {
+			req.SetBasicAuth(h.username, h.password)
+		}
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("failed to send log to Loki: %s", err.Error())
+			return
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusNoContent {
+			fmt.Printf("loki responded with status: %s", resp.Status)
+			return
+		}
+	}()
 	return nil
 }
 
